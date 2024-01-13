41598f30f68237d1ed6908ea99821257310b4135
ValiLuca <vali.luca39@gmail.com>
2024-01-13 22:16:00 +0200

    Creare pagina de adaugare user, PasswordBean SHA-256

--- src/main/java/com/librarie/proiectlibrarie/ejb/UserBean.java
+++ src/main/java/com/librarie/proiectlibrarie/ejb/UserBean.java
@@ -4,13 +4,16 @@ package com.librarie.proiectlibrarie.ejb;
 import com.librarie.proiectlibrarie.common.UserDto;
 import com.librarie.proiectlibrarie.entities.Imprumutare;
 import com.librarie.proiectlibrarie.entities.User;
+import com.librarie.proiectlibrarie.entities.UserGroup;
 import jakarta.ejb.EJBException;
 import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -22,6 +25,8 @@ public class UserBean {
     @PersistenceContext
     EntityManager entityManager;
 
+    @Inject
+    PasswordBean passwordBean;
 
     public List<UserDto> findAllUsers()
     {
@@ -35,6 +40,25 @@ public class UserBean {
         catch (Exception ex)
         {
             throw new EJBException(ex);
+        }
+    }
+
+    public void createUser(String username, String email, String password, Collection<String> groups) {
+        LOG.info("createUser");
+        User newUser = new User();
+        newUser.setUsername(username);
+        newUser.setEmail(email);
+        newUser.setPassword(passwordBean.convertToSha256(password));
+        entityManager.persist(newUser);
+        assignGroupsToUser(username, groups);
+    }
+    private void assignGroupsToUser(String username, Collection<String> groups) {
+        LOG.info("assignGroupsToUser");
+        for (String group : groups) {
+            UserGroup userGroup = new UserGroup();
+            userGroup.setUsername(username);
+            userGroup.setUserGroup(group);
+            entityManager.persist(userGroup);
         }
     }
 
